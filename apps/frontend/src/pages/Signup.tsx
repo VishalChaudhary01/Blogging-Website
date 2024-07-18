bdcb8eda74a5fcd504782fc6a3308ff02bac5b70
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-19 02:59:54 +1300

    signup page

--- apps/frontend/src/pages/Signup.tsx
+++ apps/frontend/src/pages/Signup.tsx
@@ -1,9 +1,15 @@
-
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
 export const SignupPage = () => {
      return (
-          <div>
-               Signuppage
+          <div className="grid grid-cols-1 lg:grid-cols-2">
+               <div>
+                    <Auth type="signup"/>
+               </div>
+               <div className="invisible lg:visible">
+                    <Quote />
+               </div>
           </div>
      )
 }
