71840f2d33d619b2097a2ac9d2d4f33aba76ac5a
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-19 02:14:58 +1300

    add cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors'
 import { userRoutes } from './routes/userRoutes';
 import { blogRoutes } from './routes/blogRoutes';
 
@@ -8,6 +9,8 @@ const app = new Hono<{
 		JWT_SECRET: string;
 	}
 }>();
+
+app.use('/*', cors())
 
 app.route('/api/v1/user', userRoutes);
 app.route('/api/v1/blog', blogRoutes);
