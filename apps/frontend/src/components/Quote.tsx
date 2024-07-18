b75e08e412f15417a2f246408c3187bb7714e785
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-19 03:03:59 +1300

    quote

---
+++ apps/frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+
+export const Quote = () => {
+     return (
+          <div className="bg-slate-100 h-screen flex flex-col justify-center">
+               <div className="flex justify-center">
+                    <div className="max-w-lg">
+                         <div className="text-3xl font-bold">
+                              "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                         </div>
+                         <div className="max-w-lg pt-4 font-medium">
+                              Julies Winfield
+                         </div>
+                         <div className="max-w-lg font-light text-slate-400">
+                              CEO | Vishal
+                         </div>
+                    </div>  
+               </div>
+          </div>
+     )
+}
