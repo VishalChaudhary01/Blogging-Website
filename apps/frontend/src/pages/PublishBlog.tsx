a8c206dcc4e67e1d13e3abc3037baa3d026e52d5
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-20 14:41:41 +1300

    publish blog page

---
+++ apps/frontend/src/pages/PublishBlog.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Appbar } from "../components/Appbar";
+import { BACKEND_URL } from "../config";
+
+export const PublishBlog = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  async function handleSubmit() {
+     try {
+          console.log(localStorage.getItem("token"))
+          const response = await axios.post(`${BACKEND_URL}/api/v1/blog`, {
+               title,
+               content
+          },{
+               headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+               }
+          });
+          navigate(`/blogs/${response.data.id}`)
+     } catch (e) {
+          console.log("Error while publish", e)
+          alert(e)
+     }
+  }
+
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center w-full">
+        <div className="max-w-screen-lg w-full">
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="border w-full py-1 px-2 mt-10 my-2 focus:outline-none"
+          />
+          <textarea
+            placeholder="Write content..."
+            rows={5}
+            onChange={(e) =>{
+               setContent(e.target.value)
+            }}
+            className="w-full border py-1 px-2 focus:outline-none"
+            required
+          ></textarea>
+          <button 
+            onClick={handleSubmit}
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-1 focus:ring-blue-800 font-medium rounded text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
+            Publish blog
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
