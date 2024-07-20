e8ee0df15a8f6dc714dcd992df6d142fe25af44d
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-07-20 14:39:16 +1300

    all blogs page

---
+++ apps/frontend/src/pages/Blogs.tsx
@@ -0,0 +1,36 @@
+import { Appbar } from "../components/Appbar";
+import { BlogCard } from "../components/BlogCard";
+import { useBlogs } from "../hooks";
+import { BlogCardSkeleton } from "../skeletons/BlogCardSkeleton";
+
+export const BlogsPage = () => {
+  const { blogs, loading } = useBlogs();
+
+  return (
+    <>
+      <Appbar />
+      { loading? (
+        <div>
+          <BlogCardSkeleton />
+          <BlogCardSkeleton />
+          <BlogCardSkeleton />
+          <BlogCardSkeleton />
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <div>
+            {blogs.map((blog) => (
+              <BlogCard
+                id={blog.id}
+                title={blog.title}
+                author={blog.author.name}
+                publishDate={blog.publishDate.split('T')[0]}
+                content={blog.content}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
